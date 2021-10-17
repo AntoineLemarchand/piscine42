75c2fcc5b3abe4dcf95bbd398f1468686a7f99d4
Antoine Lemarchand <alemarch@e2r10p8.clusters.42paris.fr>
2021-10-17 14:20:01 +0200

    removed .git conflict

---
+++ rush0/ex00/rush01.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush01.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alemarch <alemarch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/16 14:21:30 by alemarch          #+#    #+#             */
+/*   Updated: 2021/10/16 14:21:31 by alemarch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_put_hbar(int x, char left, char right)
+{
+	int	i;
+
+	i = 0;
+	if (x > 0)
+		ft_putchar(left);
+	if (x > 2)
+	{
+		while (i < x - 2)
+		{
+			ft_putchar('*');
+			i++;
+		}
+	}
+	if (x > 1)
+		ft_putchar(right);
+	if (x > 0)
+		ft_putchar('\n');
+}
+
+void	ft_put_vbars(int x)
+{
+	int	i;
+
+	i = 0;
+	if (x > 0)
+		ft_putchar('*');
+	while (i < x - 2)
+	{
+		ft_putchar(' ');
+		i++;
+	}
+	if (x > 1)
+		ft_putchar('*');
+	if (x > 0)
+		ft_putchar('\n');
+}
+
+void	rush(int x, int y)
+{
+	int	i;
+
+	i = 0;
+	if (y > 0)
+		ft_put_hbar(x, '/', '\\');
+	while (i < y - 2)
+	{
+		ft_put_vbars(x);
+		i++;
+	}
+	if (y > 1)
+		ft_put_hbar(x, '\\', '/');
+}
---
+++ rush0/ex00/rush04.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush04.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alemarch <alemarch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/16 14:21:53 by alemarch          #+#    #+#             */
+/*   Updated: 2021/10/16 14:21:54 by alemarch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_put_hbar(int x, char left, char right)
+{
+	int	i;
+
+	i = 0;
+	if (x > 0)
+		ft_putchar(left);
+	if (x > 2)
+	{
+		while (i < x - 2)
+		{
+			ft_putchar('B');
+			i++;
+		}
+	}
+	if (x > 1)
+		ft_putchar(right);
+	if (x > 0)
+		ft_putchar('\n');
+}
+
+void	ft_put_vbars(int x)
+{
+	int	i;
+
+	i = 0;
+	if (x > 0)
+		ft_putchar('B');
+	while (i < x - 2)
+	{
+		ft_putchar(' ');
+		i++;
+	}
+	if (x > 1)
+		ft_putchar('B');
+	if (x > 0)
+		ft_putchar('\n');
+}
+
+void	rush(int x, int y)
+{
+	int	i;
+
+	i = 0;
+	if (y > 0)
+		ft_put_hbar(x, 'A', 'C');
+	while (i < y - 2)
+	{
+		ft_put_vbars(x);
+		i++;
+	}
+	if (y > 1)
+		ft_put_hbar(x, 'C', 'A');
+}
