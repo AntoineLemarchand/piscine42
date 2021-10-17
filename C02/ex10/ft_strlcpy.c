79654aff647d6e8e9ba47785d1e9e1caa7897914
Antoine Lemarchand <alemarch@e2r10p8.clusters.42paris.fr>
2021-10-17 15:52:15 +0200

    cleanup

--- C02/ex10/ft_strlcpy.c
+++ C02/ex10/ft_strlcpy.c
@@ -6,19 +6,9 @@
 /*   By: alemarch <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/15 09:18:21 by alemarch          #+#    #+#             */
-/*   Updated: 2021/10/17 15:39:30 by alemarch         ###   ########.fr       */
+/*   Updated: 2021/10/17 15:50:14 by alemarch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-int	ft_strlen(char *str)
-{
-	int	len;
-
-	len = 0;
-	while (str[len])
-		len++;
-	return (len);
-}
 
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
